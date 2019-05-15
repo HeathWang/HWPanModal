0cfedea04534d58275f7c9f0c43fb39120008282
heathwang <yishu.jay@gmail.com>
2019-05-15 16:08:02 +0800

    docs: comment changed

--- Example/HWPanModal/PanController/Stacked/HWColorDetailViewController.m
+++ Example/HWPanModal/PanController/Stacked/HWColorDetailViewController.m
@@ -3,7 +3,7 @@
 //  HWPanModal_Example
 //
 //  Created by heath wang on 2019/5/6.
-//  Copyright © 2019 wangcongling. All rights reserved.
+//  Copyright © 2019 heath wang. All rights reserved.
 //
 
 #import "HWColorDetailViewController.h"
